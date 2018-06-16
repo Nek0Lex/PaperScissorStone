b139a46e89e513215f22b4ad6340eec68dd471dd
Nek0Lex <wonghoming111105@gmail.com>
2018-06-17 05:22:17 +0800

    attempt to try register function

--- app/src/main/java/com/example/alex/paperscissorstone/RegisterActivity.java
+++ app/src/main/java/com/example/alex/paperscissorstone/RegisterActivity.java
@@ -1,33 +1,58 @@
 package com.example.alex.paperscissorstone;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
+
 
 import static java.util.Calendar.getInstance;
 
 public class RegisterActivity extends AppCompatActivity{
     Calendar myCalendar;
+    EditText name;
     EditText dob;
-    DatePickerDialog dialog;
+    EditText phoneNum;
+    AutoCompleteTextView email;
+    TextView test;
+
+//    String inputName= name.getText().toString();
+//    String inputEmail = email.getText().toString();
+//    String inputDob = dob.getText().toString();
+//    String inputPhone = phoneNum.getText().toString();
+
     final Calendar c = Calendar.getInstance();
     int mYear = c.get(Calendar.YEAR);
     int mMonth = c.get(Calendar.MONTH);
     int mDay = c.get(Calendar.DAY_OF_MONTH);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         myCalendar = getInstance();
-        dob = (EditText) findViewById(R.id.dob);
+        dob = findViewById(R.id.dob);
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, emailDomain);
+        email = findViewById(R.id.email);
+        email.setAdapter(adapter);
+        name = findViewById(R.id.name);
+        phoneNum = findViewById(R.id.phoneNum);
+        test = findViewById(R.id.test);
 
         dob.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -41,16 +66,33 @@ public class RegisterActivity extends AppCompatActivity{
                 }, mYear,mMonth, mDay).show();
             }
         });
+
+        SharedPreferences settings = getSharedPreferences("registerPref", 0);
+
     }
+
+    private static final String[] emailDomain = new String[] {
+             "@gmail.com", "@yahoo.com","@hotmail.com","@live.com", "@facebook.com", "@msn.com","@qq.com", "@naver.com"
+    };
 
     private String setDateFormat(int year,int monthOfYear,int dayOfMonth){
-        return String.valueOf(year) + "/"
-                + String.valueOf(monthOfYear + 1) + "/"
-                + String.valueOf(dayOfMonth);
+        return String.valueOf(monthOfYear + 1) + "/"
+                + String.valueOf(dayOfMonth)+ "/"
+                +String.valueOf(year);
     }
 
 
-
+    public void register(View view) {
+        try {
+            String inputName = name.getText().toString();
+            String inputEmail = email.getText().toString();
+            String inputDob = dob.getText().toString();
+            String inputPhone = phoneNum.getText().toString();
+            test.setText(inputName);
+        } catch (Exception e){
+            Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
 }
 
 
